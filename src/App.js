d86c27b888f95aaf5d91acfb6b7e59ce77a886ad
Fraisin <appletree199@gmail.com>
2020-05-03 01:53:57 -0700

    Added reset array functionality.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import Selector from "./Selector";
-import Slider from "./Slider";
 import SortVisualizer from "./SortVisualizer/SortVisualizer";
 
 function App() {
   return (
     <div>
       <Selector />
-      <Slider />
       <SortVisualizer></SortVisualizer>
     </div>
   );
--- src/SortVisualizer/SortVisualizer.js
+++ src/SortVisualizer/SortVisualizer.js
@@ -1,19 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Slider from "@material-ui/core/Slider";
+import Button from "@material-ui/core/Button";
+import RefreshIcon from "@material-ui/icons/Autorenew";
 import "./SortVisualizer.css";
 
 const maxArrayValue = 500;
-const defaultArraySize = 100;
+
+const PrettoSlider = withStyles({
+  root: {
+    color: "#52af77",
+    height: 8
+  },
+  thumb: {
+    height: 24,
+    width: 24,
+    backgroundColor: "#fff",
+    border: "2px solid currentColor",
+    marginTop: -8,
+    marginLeft: -12,
+    "&:focus, &:hover, &$active": {
+      boxShadow: "inherit"
+    }
+  },
+  active: {},
+  valueLabel: {
+    left: "calc(-50% + 4px)"
+  },
+  track: {
+    height: 8,
+    borderRadius: 4
+  },
+  rail: {
+    height: 8,
+    borderRadius: 4
+  }
+})(Slider);
 
 export default class SortVisualizer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      currArraySize: 100,
       array: []
     };
   }
 
   componentDidMount() {
-    this.refillArray(defaultArraySize);
+    this.refillArray(this.state.currArraySize);
   }
 
   refillArray(length) {
@@ -24,21 +58,61 @@ export default class SortVisualizer extends React.Component {
     this.setState({ array });
   }
 
+  handleChange = (event, newValue) => {
+    this.setState({ currArraySize: newValue });
+    this.refillArray(newValue);
+  };
+
   render() {
     const { array } = this.state;
-
     return (
-      <div className="barContainer">
-        {array.map((value, index) => (
-          <div
-            className="arrayBar"
-            key={index}
-            style={{
-              width: `${75 / array.length}%`,
-              height: `${value / (maxArrayValue / 100)}%`
+      <div>
+        <div className="slider">
+          <h3>Control Array Size</h3>
+          <PrettoSlider
+            valueLabelDisplay="auto"
+            aria-label="Array Size"
+            defaultValue={100}
+            max={500}
+            min={5}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div className="btnsWrapper">
+          <Button
+            variant="contained"
+            color="secondary"
+            size="large"
+            startIcon={<RefreshIcon />}
+            className="mainBtn"
+            onClick={() => {
+              this.refillArray(this.state.currArraySize);
             }}
-          ></div>
-        ))}
+          >
+            Generate New Array
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            startIcon={<RefreshIcon />}
+            className="mainBtn"
+          >
+            Sort
+          </Button>
+        </div>
+        <div className="barContainer">
+          {array.map((value, index) => (
+            <div
+              className="arrayBar"
+              key={index}
+              style={{
+                width: `${75 / array.length}%`,
+                height: `${value / (maxArrayValue / 100)}%`
+              }}
+            ></div>
+          ))}
+        </div>
       </div>
     );
   }
